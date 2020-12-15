be4c3079dd59798ce2d179c8275d354c44f45d7e
araab <abdelkader-externe.raab@dalkia.fr>
2020-12-15 11:50:13 +0100

    First commit

---
+++ src/main/java/fr/dalkia/hibernatejpademo/HibernateJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package fr.dalkia.hibernatejpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateJpaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/dalkia/hibernatejpademo/HibernateJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dalkia.hibernatejpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
